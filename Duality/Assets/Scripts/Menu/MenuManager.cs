f7e36d8a2e70f51b663613400a8d00c6c9a1a04e
LQMemer <42935807+MemerRedeemer@users.noreply.github.com>
2022-01-30 16:35:57 +0100

    Additions

--- Duality/Assets/Scripts/Menu/MenuManager.cs
+++ Duality/Assets/Scripts/Menu/MenuManager.cs
@@ -8,8 +8,6 @@ public class MenuManager : MonoBehaviour
 {
     [SerializeField] GameObject _start, _credits, _settings, _coverThingy;
     [SerializeField] Camera _mainCamera;
-    float _coverThingyScale = 7;
-
 
     void Start()
     {
@@ -29,11 +27,13 @@ public class MenuManager : MonoBehaviour
     public void Setting()
     {
         ChangeFromMain();
+        Invoke(nameof(ActivateSettings), 0.5f + 0.69f);
     }
 
     public void Credits()
     {
-
+        ChangeFromMain();
+        Invoke(nameof(ActivateCredits), 0.5f + 0.69f);
     }
 
     public void ExitGame()
@@ -42,14 +42,45 @@ public class MenuManager : MonoBehaviour
         Application.Quit();
     }
 
+    public void BackToMain()
+    {
+        ChangeFromMain();
+        Invoke(nameof(ActiveStart), 0.5f + 0.69f);
+    }
+
     void ChangeFromMain()
     {
         _coverThingy.SetActive(true);
         _coverThingy.GetComponent<Animator>().Play("coverBlack");
+        Invoke(nameof(InvertColor), 0.5f);
+        Invoke(nameof(DeactivateThingy), 0.5f + 0.69f);
     }
 
-    void ChangeToWhite()
+    void InvertColor()
     {
+        GetComponent<InvertColors>().InvertColorsBW();
+    }
 
+    void DeactivateThingy()
+    {
+        _coverThingy.SetActive(false);
+        _start.SetActive(false);
+    }
+
+    void ActivateSettings()
+    {
+        _settings.SetActive(true);
+    }
+
+    void ActivateCredits()
+    {
+        _credits.SetActive(true);
+    }
+
+    void ActiveStart()
+    {
+        _settings.SetActive(false);
+        _credits.SetActive(false);
+        _start.SetActive(true);
     }
 }
